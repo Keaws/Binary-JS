a74cff9c2b1d6b5e14f80dacea27ccca8f013969
Vitalii Kalchuk <v.sweak@gmail.com>
2015-06-12 20:42:35 +0300

    Formatted code

--- Lecture-4/main.js
+++ Lecture-4/main.js
@@ -3,9 +3,15 @@
  */
 var ENTER_KEY = 13;
 
-$(document).ready(function() {
+$(document).ready(function () {
+    //remove checkmark on page refresh
+    $('#strike').prop("checked", false);
+
+    //clear input field on page refresh
+    $('#goodsText').val('');
+
     //add item
-    $('#goodsText').keypress(function(e) {
+    $('#goodsText').keypress(function (e) {
         if (e.which === ENTER_KEY && $('#goodsText').val() !== '') {
             var input = $('#goodsText').val();
             $('#goodsList').append('<li> <input type="checkbox" class="toggle" /><span>' +
@@ -15,13 +21,13 @@ $(document).ready(function() {
     });
 
     //delete item
-    $(document).on('click', '.delete', function() {
+    $(document).on('click', '.delete', function () {
         $(this).closest('li').remove();
     });
 
     //strikethrough item
-    $(document).on('change', '.toggle', function() {
-        if(this.checked) {
+    $(document).on('change', '.toggle', function () {
+        if (this.checked) {
             $(this).closest('li').addClass('strikethrough');
         }
         else {
@@ -30,22 +36,36 @@ $(document).ready(function() {
     });
 
     //strikethrough all
-    $('#strike').click(function(){
-        if($(this).prop("checked")) {
+    $('#strike').click(function () {
+        if ($(this).prop("checked")) {
             $(".toggle").prop("checked", true);
             $('li').addClass('strikethrough');
         } else {
             $(".toggle").prop("checked", false);
             $('li').removeClass('strikethrough');
-        }                
+        }
     });
 
     //delete all strikethrough
-    $('#strikeDel').click(function() {
+    $('#strikeDel').click(function () {
         $('li.strikethrough').remove();
         $('#strike').prop("checked", false);
     });
 
-    //remove checkmark when page is refreshed
-    $('#strike').prop("checked", false);
+    //edit item
+    $(document).on('dblclick', 'li span', function () {
+        var $liToEdit = $(this).closest('li span');
+        $liToEdit.addClass('edititng');
+        var textToEdit = $liToEdit.html();
+        $('#goodsText').val(textToEdit);
+        $('#goodsText').focus();
+
+        $('#goodsText').keypress(function (e) {
+            if (e.which === ENTER_KEY) {
+                var input = $('#goodsText').val();
+                $liToEdit.val(input);
+            }
+        });
+    });
+
 });
