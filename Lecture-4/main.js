a0fb993821cf1cb54dd2a7e1e4fdb1736fee8112
Vitalii Kalchuk <v.sweak@gmail.com>
2015-06-13 14:31:22 +0300

    Code structure updates

--- Lecture-4/main.js
+++ Lecture-4/main.js
@@ -18,10 +18,12 @@ $(document).ready(function () {
     $('#goodsText').keypress(function (e) {
         if (e.which === ENTER_KEY && $('#goodsText').val() !== '') {
             var input = $('#goodsText').val();
-            $('#goodsList').append('<li> <input type="checkbox" class="toggle" />' +
+            $('#goodsList').append('<li>' +
+                '<input type="checkbox" class="toggle" />' +
                 '<label class="display">' + input + '</label>' +
-                '<input type="text" class="edit" maxlength="15"/>' +
-                '<a class="delete"></a></li>');
+                '<input type="text" class="edit" maxlength="25"/>' +
+                '<a class="delete"></a>' +
+                '</li>');
             $('#goodsText').val('');
         }
     });
@@ -33,12 +35,8 @@ $(document).ready(function () {
 
     //strikethrough item
     $(document).on('change', '.toggle', function () {
-        if (this.checked) {
-            $(this).closest('li').addClass('strikethrough');
-        }
-        else {
-            $(this).closest('li').removeClass('strikethrough');
-        }
+        if (this.checked) $(this).closest('li').addClass('strikethrough');
+        else $(this).closest('li').removeClass('strikethrough');
     });
 
     //strikethrough all
@@ -65,18 +63,11 @@ $(document).ready(function () {
         $(this).hide().siblings(".edit").show().val($(this).text()).focus();
     });
 
-    $(document).on('keyup', '.edit', function(e) {
+    $(document).on('keyup', '.edit', function (e) {
         if (e.which === ENTER_KEY && $('#edit').val() !== '') {
             $(this).hide().siblings(".display").show().text($(this).val());
         } else if (e.which === ESCAPE_KEY) {
             $(this).hide().siblings(".display").show().text(originalText);
         }
     });
-
-    //show delete button
-/*    $(document).on('mouseenter', 'li label', function () {
-        $(this).siblings('.delete').fadeIn();
-    }).on('mouseleave', 'li label', function () {
-        $(this).siblings('.delete').fadeOut();
-    });*/
 });
